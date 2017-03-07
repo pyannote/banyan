c5a42e6c34b01538ca379e0aa1685a1403616f15
Hervé BREDIN <bredin@limsi.fr>
2017-03-07 17:03:17 -0600

    fix: revert breaking change

--- banyan/_int_imp/_build_tree_imp.cpp
+++ banyan/_int_imp/_build_tree_imp.cpp
@@ -1,5 +1,4 @@
 #include <Python.h>
-#include <iterator>
 #include "rank_metadata.hpp"
 #include "min_gap_metadata.hpp"
 #include "overlapping_intervals_metadata.hpp"
@@ -17,7 +16,7 @@ using namespace std;
 
 template<
     class Alg_Tag,
-    typename Key_Type, 
+    typename Key_Type,
     bool Set,
     class MetadataTag,
     class LT>
@@ -28,7 +27,7 @@ struct _KnownMappingBuilder
 
 template<
     class Alg_Tag,
-    typename Key_Type, 
+    typename Key_Type,
     class MetadataTag,
     class LT>
 struct _KnownMappingBuilder<
@@ -40,11 +39,11 @@ struct _KnownMappingBuilder<
 {
     static _TreeImpBase *
     build_imp(PyObject * seq, PyObject * metadata, PyObject * lt)
-    {    
+    {
         return new _DictTreeImp<
-            Alg_Tag, 
-            Key_Type, 
-            MetadataTag, 
+            Alg_Tag,
+            Key_Type,
+            MetadataTag,
             LT>(seq, metadata, lt);
     }
 };
@@ -65,9 +64,9 @@ struct _KnownMappingBuilder<
     build_imp(PyObject * seq, PyObject * metadata, PyObject * lt)
     {
         return new _SetTreeImp<
-            Alg_Tag, 
-            Key_Type, 
-            MetadataTag, 
+            Alg_Tag,
+            Key_Type,
+            MetadataTag,
             LT>(seq, metadata, lt);
     }
 };
@@ -76,7 +75,7 @@ template<
     class Alg_Tag,
     class Key_Type,
     class MetadataTag,
-    class LT>   
+    class LT>
 struct _KnownMetadataBuilder
 {
     static _TreeImpBase *
@@ -86,8 +85,8 @@ struct _KnownMetadataBuilder
         if (mapping == 0)
             return _KnownMappingBuilder<Alg_Tag, Key_Type, false, MetadataTag, LT>::build_imp(
                 seq, NULL, lt);
-#endif // #ifndef BANYAN_MIN_MAPPING_BUILD                
-        if (mapping == 1)            
+#endif // #ifndef BANYAN_MIN_MAPPING_BUILD
+        if (mapping == 1)
             return _KnownMappingBuilder<Alg_Tag, Key_Type, true, MetadataTag, LT>::build_imp(
                 seq, NULL, lt);
         DBG_VERIFY(false);
@@ -98,7 +97,7 @@ struct _KnownMetadataBuilder
 template<
     class Alg_Tag,
     class Key_Type,
-    class LT>   
+    class LT>
 struct _KnownMetadataBuilder<
     Alg_Tag,
     Key_Type,
@@ -113,7 +112,7 @@ struct _KnownMetadataBuilder<
             return _KnownMappingBuilder<Alg_Tag, Key_Type, false, _PyObjectCBMetadataTag, LT>::build_imp(
                 seq, metadata, lt);
 #endif // #ifndef BANYAN_MIN_MAPPING_BUILD
-        if (mapping == 1)            
+        if (mapping == 1)
             return _KnownMappingBuilder<Alg_Tag, Key_Type, true, _PyObjectCBMetadataTag, LT>::build_imp(
                 seq, metadata, lt);
         DBG_VERIFY(false);
@@ -124,7 +123,7 @@ struct _KnownMetadataBuilder<
 template<
     class Alg_Tag,
     class Key_Type,
-    class LT>   
+    class LT>
 struct _KnownLTBuilder
 {
     static _TreeImpBase *
@@ -132,41 +131,41 @@ struct _KnownLTBuilder
     {
         if (metadata == Py_None)
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _NullMetadataTag, LT>::build_imp(
-                seq, mapping, lt);                
-        DBG_ASSERT(metadata != NULL && PyTuple_Check(metadata) && PyTuple_Size(metadata) == 2);        
+                seq, mapping, lt);
+        DBG_ASSERT(metadata != NULL && PyTuple_Check(metadata) && PyTuple_Size(metadata) == 2);
 
 #ifndef BANYAN_NO_UPDATORS_BUILD
         PyObject * const meta = PyTuple_GET_ITEM(metadata, 0);
         DBG_ASSERT(PyCallable_Check(meta));
         DBG_ASSERT(PyType_Check(meta));
-        PyObject * const inst = PyObject_CallFunctionObjArgs(meta, NULL);        
+        PyObject * const inst = PyObject_CallFunctionObjArgs(meta, NULL);
         if (inst == NULL) {
             PyErr_NoMemory();
             return NULL;
-        }        
-        BANYAN_PYOBJECT_DUMMY_INCREF(inst);            
+        }
+        BANYAN_PYOBJECT_DUMMY_INCREF(inst);
         if (PyObject_TypeCheck(inst, &RankMetadataType)) {
             BANYAN_PYOBJECT_DECREF(inst);
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _RankMetadataTag, LT>::build_imp(
-                seq, mapping, lt);                
+                seq, mapping, lt);
         }
         if (PyObject_TypeCheck(inst, &MinGapMetadataType)) {
             BANYAN_PYOBJECT_DECREF(inst);
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _MinGapMetadataTag, LT>::build_imp(
-                seq, mapping, lt);                
+                seq, mapping, lt);
         }
         if (PyObject_TypeCheck(inst, &OverlappingIntervalsMetadataType)) {
             BANYAN_PYOBJECT_DECREF(inst);
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _IntervalMaxMetadataTag, LT>::build_imp(
-                seq, mapping, lt);                
+                seq, mapping, lt);
         }
 
         BANYAN_PYOBJECT_DECREF(inst);
 #endif // #ifndef BANYAN_NO_UPDATORS_BUILD
 
         return _KnownMetadataBuilder<Alg_Tag, Key_Type, _PyObjectCBMetadataTag, LT>::build_imp(
-            seq, mapping, metadata, lt);                
-    }            
+            seq, mapping, metadata, lt);
+    }
 };
 
 template<
@@ -180,39 +179,39 @@ struct _Knownkey_typeBuilder
         if (metadata == Py_None)
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _NullMetadataTag, std::less<Key_Type> >::build_imp(
                 seq, mapping, NULL);
-        DBG_ASSERT(metadata != NULL && PyTuple_Check(metadata) && PyTuple_Size(metadata) == 2);        
+        DBG_ASSERT(metadata != NULL && PyTuple_Check(metadata) && PyTuple_Size(metadata) == 2);
 
 #ifndef BANYAN_NO_UPDATORS_BUILD
         PyObject * const meta = PyTuple_GET_ITEM(metadata, 0);
         DBG_ASSERT(PyCallable_Check(meta));
         DBG_ASSERT(PyType_Check(meta));
-        PyObject * const inst = PyObject_CallFunctionObjArgs(meta, NULL);        
+        PyObject * const inst = PyObject_CallFunctionObjArgs(meta, NULL);
         if (inst == NULL) {
             PyErr_NoMemory();
             return NULL;
-        }        
-        BANYAN_PYOBJECT_DUMMY_INCREF(inst);            
+        }
+        BANYAN_PYOBJECT_DUMMY_INCREF(inst);
         if (PyObject_TypeCheck(inst, &RankMetadataType)) {
             BANYAN_PYOBJECT_DECREF(inst);
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _RankMetadataTag, std::less<Key_Type> >::build_imp(
-                seq, mapping, NULL);                
+                seq, mapping, NULL);
         }
         if (PyObject_TypeCheck(inst, &MinGapMetadataType)) {
             BANYAN_PYOBJECT_DECREF(inst);
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _MinGapMetadataTag, std::less<Key_Type> >::build_imp(
-                seq, mapping, NULL);                
+                seq, mapping, NULL);
         }
         if (PyObject_TypeCheck(inst, &OverlappingIntervalsMetadataType)) {
             BANYAN_PYOBJECT_DECREF(inst);
             return _KnownMetadataBuilder<Alg_Tag, Key_Type, _IntervalMaxMetadataTag, std::less<Key_Type> >::build_imp(
-                seq, mapping, NULL);                
+                seq, mapping, NULL);
         }
 
         BANYAN_PYOBJECT_DECREF(inst);
 #endif // #ifndef BANYAN_NO_UPDATORS_BUILD
 
         return _KnownMetadataBuilder<Alg_Tag, Key_Type, _PyObjectCBMetadataTag, std::less<Key_Type> >::build_imp(
-            seq, mapping, metadata, NULL);                
+            seq, mapping, metadata, NULL);
     }
 };
 
@@ -228,12 +227,12 @@ struct _Knownkey_typeBuilder<
         DBG_ASSERT(key == Py_None || cmp == Py_None);
 #if PY_MAJOR_VERSION >= 3
         // Tmp Ami - make a static warning function.
-        if (cmp != Py_None) 
+        if (cmp != Py_None)
             _py_warn(PyExc_DeprecationWarning, "cmp function deprecated in favour of key function");
 #endif // #if PY_MAJOR_VERSION >= 3
 
         // Tmp Ami - who checks if callable? Should not be in assert.
-        if (key != Py_None) 
+        if (key != Py_None)
             return _KnownLTBuilder<Alg_Tag, PyObject *, _PyObjectKeyCBLT>::build_imp(
                 seq, mapping, metadata, key);
         if (cmp != Py_None)
@@ -253,7 +252,7 @@ static bool _is_long_type(PyObject * type)
         BANYAN_PYOBJECT_DECREF(inst);
         return false;
     }
-    
+
 #if PY_MAJOR_VERSION >= 3
     if (PyLong_CheckExact(inst)) {
 #else // #if PY_MAJOR_VERSION >= 3
@@ -275,7 +274,7 @@ static bool _is_double_type(PyObject * type)
         BANYAN_PYOBJECT_DECREF(inst);
         return false;
     }
-    
+
     if (PyFloat_CheckExact(inst)) {
         BANYAN_PYOBJECT_DECREF(inst);
         return true;
@@ -293,11 +292,11 @@ static bool _is_string_type(PyObject * type)
         BANYAN_PYOBJECT_DECREF(inst);
         return false;
     }
-    
+
 #if PY_MAJOR_VERSION >= 3
     if (PyByteArray_CheckExact(inst)) {
 #else // #if PY_MAJOR_VERSION >= 3
-    if (PyString_CheckExact(inst)) {       
+    if (PyString_CheckExact(inst)) {
 #endif // #if PY_MAJOR_VERSION >= 3
         BANYAN_PYOBJECT_DECREF(inst);
         return true;
@@ -316,7 +315,7 @@ static bool _is_unicode_string_type(PyObject * type)
         BANYAN_PYOBJECT_DECREF(inst);
         return false;
     }
-    
+
     if (PyUnicode_CheckExact(inst)) {
         BANYAN_PYOBJECT_DECREF(inst);
         return true;
@@ -332,44 +331,44 @@ struct _KnownAlgBuilder
     static _TreeImpBase *
     build_imp(
         PyObject * seq,
-        PyObject * key_type, 
-        int mapping, 
+        PyObject * key_type,
+        int mapping,
         PyObject * metadata,
-        PyObject * key, 
+        PyObject * key,
         PyObject * cmp)
     {
         if ((key != Py_None || cmp != Py_None) && key_type != Py_None) {
             _py_warn(
-                PyExc_Warning, 
-                "Key-type optimization cannot be performed with key function or comparison function");            
+                PyExc_Warning,
+                "Key-type optimization cannot be performed with key function or comparison function");
             key_type = Py_None;
-        }            
+        }
 
-        if (key_type == Py_None) 
+        if (key_type == Py_None)
             return _Knownkey_typeBuilder<Alg_Tag, PyObject *>::build_imp(seq, mapping, metadata, key, cmp);
 
-        DBG_ASSERT(key == Py_None);            
-        DBG_ASSERT(cmp == Py_None);            
+        DBG_ASSERT(key == Py_None);
+        DBG_ASSERT(cmp == Py_None);
 
         if (PyTuple_Check(key_type)) {
             if (PyTuple_Size(key_type) != 2) {
-                _py_warn(PyExc_Warning, "Unimplemented key-type optimization");            
+                _py_warn(PyExc_Warning, "Unimplemented key-type optimization");
                 return _Knownkey_typeBuilder<Alg_Tag, PyObject *>::build_imp(seq, mapping, metadata, key, cmp);
             }
 
             if (
-                    _is_long_type(PyTuple_GET_ITEM(key_type, 0)) && 
-                    _is_long_type(PyTuple_GET_ITEM(key_type, 1)))                                 
+                    _is_long_type(PyTuple_GET_ITEM(key_type, 0)) &&
+                    _is_long_type(PyTuple_GET_ITEM(key_type, 1)))
                 return _Knownkey_typeBuilder<Alg_Tag, std::pair<long, long> >::build_imp(seq, mapping, metadata);
             if (
-                    _is_double_type(PyTuple_GET_ITEM(key_type, 0)) && 
-                    _is_double_type(PyTuple_GET_ITEM(key_type, 1)))                                 
+                    _is_double_type(PyTuple_GET_ITEM(key_type, 0)) &&
+                    _is_double_type(PyTuple_GET_ITEM(key_type, 1)))
                 return _Knownkey_typeBuilder<Alg_Tag, std::pair<double, double> >::build_imp(seq, mapping, metadata);
 
-            _py_warn(PyExc_Warning, "Unimplemented key-type optimization");            
+            _py_warn(PyExc_Warning, "Unimplemented key-type optimization");
             return _Knownkey_typeBuilder<Alg_Tag, PyObject *>::build_imp(seq, mapping, metadata, key, cmp);
         }
-        
+
         if (_is_long_type(key_type))
             return _Knownkey_typeBuilder<Alg_Tag, long>::build_imp(seq, mapping, metadata);
         if (_is_double_type(key_type))
@@ -379,18 +378,18 @@ struct _KnownAlgBuilder
         if (_is_unicode_string_type(key_type))
             return _Knownkey_typeBuilder<Alg_Tag, UnicodeString>::build_imp(seq, mapping, metadata);
 
-        _py_warn(PyExc_Warning, "Unimplemented key-type optimization");            
+        _py_warn(PyExc_Warning, "Unimplemented key-type optimization");
         return _Knownkey_typeBuilder<Alg_Tag, PyObject *>::build_imp(seq, mapping, metadata, key, cmp);
-    }    
+    }
 };
 
 _TreeImpBase *
 _build_tree_imp(
-    int alg, 
+    int alg,
     PyObject * seq,
-    PyObject * key_type, 
+    PyObject * key_type,
     int mapping,
-    PyObject * metadata, 
+    PyObject * metadata,
     PyObject * key,
     PyObject * cmp)
 {
@@ -400,10 +399,8 @@ _build_tree_imp(
     if (alg == 1)
         return _KnownAlgBuilder<_SplayTreeTag>::build_imp(seq, key_type, mapping, metadata, key, cmp);
     if (alg == 2)
-        return _KnownAlgBuilder<_OVTreeTag>::build_imp(seq, key_type, mapping, metadata, key, cmp); 
+        return _KnownAlgBuilder<_OVTreeTag>::build_imp(seq, key_type, mapping, metadata, key, cmp);
 #endif // #ifndef BANYAN_MIN_ALG_BUILD
-    DBG_VERIFY(false);        
+    DBG_VERIFY(false);
     return (_TreeImpBase *)NULL;
 }
-
-
